80c6e811fc028dd5f79bd8e853fcc3b17f495d0f
Jayden Wise <jayden.wise@hotmail.com>
2018-03-15 16:39:30 +0100

    Added React Navigation

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import MainScreen from './src/MainScreen';
 
 export default class App extends React.Component {
   render() {
+      const MainNavigator = StackNavigator({
+          Main: { screen: MainScreen }
+      });
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+        <MainNavigator />
     );
   }
 }
