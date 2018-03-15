e8c0f58ed2fcddf88ce05a2f3973ddbfb65567dd
Jayden Wise <jayden.wise@hotmail.com>
2018-03-15 20:10:21 +0100

    Implement Heading bearings

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -12,16 +12,23 @@ class MainScreen extends React.Component {
         this._getLocationAsync();
     }
     _getLocationAsync = async () => {
+        //Ask for location permissions
     let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    //if not granted supply error message
     if (status !== 'granted') {
       this.setState({
         errorMessage: 'Permission to access location was denied',
       });
     }
-
-    let location = Location.watchHeadingAsync();
+    //Find heading and then setState
+    let location = Location.getHeadingAsync();
     this.setState({ location });
   };
+  componentDidMount() {
+      //After finding initial heading, watch for changes and set state accordingly
+      let location = Location.watchHeadingAsync();
+      this.setState({ location });
+  }
     render() {
         let text = 'Waiting..';
         if (this.state.errorMessage) {
@@ -31,12 +38,17 @@ class MainScreen extends React.Component {
         }
         return (
             <View style={{ flex: 1, backgroundColor: '#ddd' }}>
-                <Text>Hi!</Text>
-                <Text>{text}</Text>
-                <Image source={require('../assets/images/Gray_compass_rose.png')} />
+
+                <Image style={compassStyle}source={require('../assets/images/Gray_compass_rose.png')}/>
             </View>
         )
     }
 }
+
+const compassStyle = {
+    marginLeft: 10,
+    transform: [{ rotate: this.state.location}]
+ }
+
 
 export default MainScreen;
